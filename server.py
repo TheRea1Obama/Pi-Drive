5149f6db18753e6be4db64c699ea0c8a904f6e7b
danny karp <dannykarp1721@gmail.com>
2024-12-08 00:40:02 +0200

    asdasd

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, send_from_directory
 import os
 
 app = Flask(__name__)
@@ -7,27 +7,35 @@ UPLOAD_FOLDER = 'files'
 os.makedirs(UPLOAD_FOLDER, exist_ok=True)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
-
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return "<a href='/upload'>Upload</a> <a href='/download'>Download</a>"
 
 @app.route('/upload', methods=['GET', 'POST'])
 def upload_page():
     if request.method == 'POST':
-        # Check if a file was submitted
         if 'file' not in request.files:
             return "No file part"
-        file = request.files['file']
-        # Save the file to the upload folder
-        file.save(os.path.join(app.config['UPLOAD_FOLDER'], file.filename))
-        return f"File {file.filename} uploaded successfully!"
+        
+        file = request.files['file'] 
+        if file.filename == '':
+            return "No selected file"
+        # save the file 
+        file.save(dst=os.path.join(str(app.config['UPLOAD_FOLDER']), str(file.filename))) 
+        
     return render_template('upload.html')
 
 
 @app.route('/download')
-def download_page():
-    return render_template('download.html')
+def downloads():
+    # List all files in the directory
+    files = os.listdir(UPLOAD_FOLDER)
+    return render_template('download.html', files=files)
+
+# Route to serve a specific file for download
+@app.route('/files/<filename>')
+def download_file(filename):
+    return send_from_directory(UPLOAD_FOLDER, filename, as_attachment=True)
 
 if __name__ == '__main__':
     app.run(debug=True)
